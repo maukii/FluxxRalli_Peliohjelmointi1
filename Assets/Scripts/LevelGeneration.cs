c715658970f1a14557183e1321446efe4017f1d5
RiikkaKilp <32130006+RiikkaKilp@users.noreply.github.com>
2018-01-18 13:02:47 +0200

    Siivosin assests kansioo, värkkäsin levelgenerattorin kans

--- Assets/Scripts/LevelGeneration.cs
+++ Assets/Scripts/LevelGeneration.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class LevelGeneration : MonoBehaviour {
 
+    /*
+     *
+     *  Track connections
+     *  
+     *             straight01  straight02  corner01    corner02    corner03    corner04
+     *  straight01      X           -           X           X           -           -   
+     *  straight02                  fuck u talukkko
+     *  corner01
+     *  corner02
+     *  corner03
+     *  corner04
+     * 
+    */ 
+
     public int amountX;
     public int amountZ;
 
@@ -11,6 +25,7 @@ public class LevelGeneration : MonoBehaviour {
     public float spacingZ;
 
     public GameObject[] tile;
+    public GameObject startTile;
 
     void Start()
     {
@@ -24,17 +39,30 @@ public class LevelGeneration : MonoBehaviour {
         {
             for (int x = 0; x < amountZ; x++)
             {
+                int randomTile = Random.Range(0, tile.Length);
+
                 Vector3 spawnPos = new Vector3(0 + x * (1 + spacingX), 0, 0 + y * (1 + spacingZ));
-                GameObject spawnPoint = Instantiate(new GameObject(), spawnPos, Quaternion.identity);
-                //GameObject g = Instantiate(tile, spawnPos, Quaternion.identity) as GameObject;
+                //GameObject spawnPoint = Instantiate(new GameObject(), spawnPos, Quaternion.identity);
+                GameObject g = Instantiate(tile[randomTile], spawnPos, Quaternion.identity) as GameObject;
                 //g.name = x + "/" + y;
-                //g.transform.parent = gameObject.transform;
+                g.transform.parent = gameObject.transform;
+
+                /*
+                 * spawnaa tile
+                 * spawnaa uusi_tile edellisen tilen liittymiskohtaan
+                 * tilen liittymiskohta = minne auto voi mennä äksde
+                 * 
+                */
             }
         }
     }
 
-    // Update is called once per frame
-    void Update () {
-		
-	}
+    void LinearGeneration()
+    {
+        GameObject startPoint = Instantiate(startTile, new Vector3(0,0,0), Quaternion.identity);
+        int randomTile = Random.Range(0, tile.Length);
+
+        GameObject newTile = Instantiate(tile[randomTile], new Vector3(0, 0, 0), Quaternion.identity);
+        //newTile.transform.position = startPoint.transform.
+    }
 }
