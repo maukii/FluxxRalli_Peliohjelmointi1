865eadee51b54ff18c36bca9f1985dada3d3c7f8
Rarrix <32179106+Rarrix@users.noreply.github.com>
2018-02-06 14:06:41 +0200

    timers

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+	// Koodin kirjoittanut Janne Viitala 6.2.2018
+
+
+
+public class GameTimer : MonoBehaviour {
+
+    public Text gameTimerText;
+    float gameTimer=0f;
+
+
+
+
+
+	
+	// Update is called once per frame
+	void Update () {
+        gameTimer += Time.deltaTime;
+
+        int seconds = (int)(gameTimer % 60);
+        int minutes = (int)(gameTimer / 60) % 60;
+
+        string timerString = string.Format("{0:0}:{1:00}", minutes, seconds);
+        gameTimerText.text = timerString;
+
+	}
+}
---
+++ Assets/Scripts/StartCountdown.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+
+
+//Koodin kirjoittanut Janne Viitala 6.2.2018
+//
+
+
+
+
+public class StartCountdown : MonoBehaviour
+{
+    public int timeLeft = 3;
+    public Text countdownText;
+
+  
+    void Start()
+    {
+        StartCoroutine("CountDown");
+    }
+
+
+    void Update()
+    {
+        countdownText.text = ("" + timeLeft);
+
+        if (timeLeft == 0)
+        {
+            countdownText.text = "GO";
+        }
+
+
+        if (timeLeft == -1)
+        {
+            countdownText.text = ("");
+            StopCoroutine("CountDown");
+        }
+    }
+
+    IEnumerator CountDown()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            timeLeft--;
+        }
+    }
+
+}
+
