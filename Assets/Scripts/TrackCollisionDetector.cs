81846e979fddf533fa6ac71035938f213a0728e9
RiikkaKilp <32130006+RiikkaKilp@users.noreply.github.com>
2018-02-26 12:13:55 +0200

    Track Destroyer script

---
+++ Assets/Scripts/TrackCollisionDetector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackCollisionDetector : MonoBehaviour {
+
+    bool TrackCollision;
+	
+	void OnTriggerEnter (Collider other)
+    {
+        if (other.tag == "straightUp" || other.tag == "straightLeft" || other.tag == "straightRight" || other.tag == "corner01" || other.tag == "corner02" || other.tag == "corner03" || other.tag == "corner04")
+        {
+            Debug.Log("Track Collision detected");
+            other.GetComponent<Collider>().enabled = false;
+            GameObject.FindGameObjectWithTag("LevelGenerator").GetComponent<EndlessTrackGenerator>().NewChunkOnTrigger();
+        }
+
+        else
+        {
+            Debug.Log("Unknown Collision");
+        }
+	}
+}
